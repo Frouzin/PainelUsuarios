cd25584d5516fd24055a32dd9137efec00711975
Frouzin <matheusereis44@gmail.com>
2025-05-26 15:47:27 -0300

    logica e Html finalizado

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,47 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+
+interface Usuario {
+  nome: string;
+  idade: number;
+  profissao: string;
+  email: string;
+  status: boolean;
+  mostrar?: boolean; 
+}
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, CommonModule, FormsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'PainelUsuarios';
+  title = 'Painel de Usuarios';
+
+
+  usuarios: Usuario[] = [];
+
+  nome: string = '';
+  idade: number = 0;
+  profissao: string = '';
+  email: string = '';
+  status: boolean = false;
+  mostrar: boolean = false;
+
+  
+  addUsuario() {
+    const novoUsuario: Usuario & { mostrar?: boolean } = {
+      nome: this.nome,
+      idade: this.idade,
+      profissao: this.profissao,
+      email: this.email,
+      status: this.status,
+      mostrar: false
+    };
+    this.usuarios.push(novoUsuario);
+  }
+  
 }
